1d539cce3ad5f08f9c35778fcded826526339463
Yeshi Tsering Gangchen <yestsering@gmail.com>
2021-05-04 11:52:30 +0200

    quiz component

--- quize-site/src/components/Menu.js
+++ quize-site/src/components/Menu.js
@@ -1,12 +1,24 @@
-import React, { useState } from "react";
-
+import React, { useContext } from "react";
+import { QuizContext } from "../Helper/Contexts";
 
 const Menu = () => {
-    
+    const { quizBoard, setQuizBoard } = useContext(QuizContext); //Destructuring the thing global variable
+
     return (
-        <div>
-            <p></p>
+        <div className="Quiz">
+            {/* When i Click the Button then  setQuizBoard change from "menu" to "quiz" in App.js as de-structure as above */}
+            <button
+                className="btn btn-dark"
+                onClick={() => {
+                    setQuizBoard('quiz')
+                }}>Start Quiz</button>
         </div>
+        // <div className="Menu">
+        //     {/* When i Click the Button then  setQuizBoard change from "menu" to "quiz" in App.js as de-structure as above */}
+        //     <button onClick={() => {
+        //         setQuizBoard('quiz')
+        //     }}>Start Quiz</button>
+        // </div>
     )
 }
 
